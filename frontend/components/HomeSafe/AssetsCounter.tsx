ba16f4f3a3a788ae0b6a8f2555e7382af45e4393
prestx00 <basaev129@gmail.com>
2023-02-16 09:34:30 +0400

    add AssetsCounter

---
+++ frontend/components/HomeSafe/AssetsCounter.tsx
@@ -0,0 +1,22 @@
+import { Row, Text } from "@nextui-org/react";
+import LinkAndCopy from "../Common/LinkAndCopy";
+
+export default function AssetsCounter() {
+  return (
+    <Row align="center" justify="center">
+      <Text b size={14} color="#666666" css={{ mr: "$5" }}>
+        Tokens
+        <Text b size={20} css={{ ml: "$5" }}>
+          0
+        </Text>
+      </Text>
+      <Text b size={14} color="#666666" css={{ mr: "$10" }}>
+        NFT
+        <Text b size={20} css={{ ml: "$5" }}>
+          0
+        </Text>
+      </Text>
+      <LinkAndCopy />
+    </Row>
+  );
+}
