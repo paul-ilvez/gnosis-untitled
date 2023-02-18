3cbbeb66e27b1e09d8b4c6538ceaa5ef29deb6c9
flame1800 <ruslan08022201@gmail.com>
2023-02-18 09:26:55 +0500

    change ux form create safe

--- frontend/components/CreateSafe/CreateSafe.tsx
+++ frontend/components/CreateSafe/CreateSafe.tsx
@@ -1,38 +1,18 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext } from "react";
 import Layout from "@/components/Layout";
 import SetOwners from "@/components/LoadSafe/Steps/SetOwners";
 import Review from "@/components/LoadSafe/Steps/Review";
-import InitSafe from "@/components/CreateSafe/Steps/InitSafe";
 import { Screens } from "@/global";
-import { Contract, JsonRpcProvider } from "ethers";
-import { SafeFactory } from "@/abi/SafeFactory";
 import { AppContext, AppContextData } from "@/store/AppContext";
 
 const screens: Screens = {
-  init: <InitSafe />,
   owners: <SetOwners />,
   review: <Review />,
 };
 
 const CreateSafe = () => {
   const appCtx = useContext<AppContextData>(AppContext);
-
   const screen = screens[appCtx.createSafeStatus.status as keyof Screens];
-
-  useEffect(() => {
-    (async () => {
-      const url = "http://127.0.0.1:8545";
-      const customHttpProvider = new JsonRpcProvider(url);
-
-      const safeFactory = new Contract(
-        "0x5fbdb2315678afecb367f032d93f642f64180aa3",
-        SafeFactory,
-        customHttpProvider
-      );
-
-      console.log("contract: ", safeFactory);
-    })();
-  }, []);
 
   return <Layout>{screen}</Layout>;
 };
